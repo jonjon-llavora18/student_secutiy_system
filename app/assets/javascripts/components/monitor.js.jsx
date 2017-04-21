152e9e2b4e1f3f0959f1c513b8dc26b0308269f5
Argel <timothy.llavora@bibo.com.ph>
2017-04-21 19:39:07 +0800

    fixed conflict

--- app/assets/javascripts/components/monitor.js.jsx
+++ app/assets/javascripts/components/monitor.js.jsx
@@ -27,20 +27,35 @@ var Monitor = React.createClass({
 
 	render: function() {
 		return (
-			<div>
-			  <h1 className="home-header homeHeader">Student Security System</h1>
+			<h1 class="home-header homeHeader">Student Security System</h1>
 
-			  <div className="student-information clearfix">
-			    <div className="student-photo student-information-div">
-			      <img src="../assets/dummy_101.jpg" className="student-photo-img" />
-			    </div>
-			    <div className="student-text-info student-information-div">
-			      <span className="student-txt-inf student-name">Ivan Pilapil</span>
-			      <span className="student-txt-inf student-id">F100033890</span>
-			      <span className="student-txt-inf student-timein">IN: 03:44:36</span>
-			    </div>
-			  </div>
-			</div>
+		  <div>
+		    <div class="home-clock"></div>
+		    <!-- <div class="school-announcement">
+		      <p class="schoolAnnouncement">Lorem Ipsum is simply dummy text of the printing and typesetting industry.</p>
+		    </div> -->
+		  </div>
+
+		  <div class="student-information clearfix">
+		    <div class="student-photo student-information-div">
+		      <%= image_tag "dummy_101.jpg", class: "student-photo-img" %>
+		    </div>
+		    <div class="student-text-info student-information-div">
+		      <span class="student-txt-inf student-name">Ivan Pilapil</span>
+		      <span class="student-txt-inf student-id">F100033890</span>
+		      <span class="student-txt-inf student-timein">IN: 03:44:36</span>
+		    </div>
+		  </div>
+
+		  <div class="swipe-error">
+		    <div class="student-photo student-information-div">
+		      <%= image_tag "error-mascott.png", class: "error-mascott" %>
+		    </div>
+		    <div class="student-text-info student-information-div">
+		      <span class="student-txt-inf student-error-1">No Student</span>
+		      <span class="student-txt-inf student-error-2">Found</span>
+		    </div>
+		  </div>
 		)
 	}
 });
