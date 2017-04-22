12ee1a8384ad449496519e29fb7dd52251bf0b60
Roi Deldig <philip.deldig@bibo.com.ph>
2017-04-22 14:42:17 +0800

    added announcement on initial jsx

--- app/assets/javascripts/components/initial_page.js.jsx
+++ app/assets/javascripts/components/initial_page.js.jsx
@@ -20,14 +20,62 @@ function digitalClock() {
 	setInterval(clock, 1000);
 }
 
+function marquee(a, b) {
+  var width = b.width();
+  var start_pos = a.width();
+  var end_pos = -width;
+
+  function scroll() {
+    if (b.position().left <= -width) {
+      b.css("left", start_pos);
+      scroll();
+    }
+    else {
+      time = (parseInt(b.position().left, 10) - end_pos) *
+        (90000 / (start_pos - end_pos));
+      b.animate({
+        "left": -width
+      }, time, "linear", function() {
+        scroll();
+      });
+    }
+  }
+
+  b.css({
+    "width": width,
+    "left": start_pos
+  });
+  scroll(a, b);
+
+  b.mouseenter(function() {
+    b.stop();
+    b.clearQueue();
+  });
+
+  b.mouseleave(function() {
+    scroll(a, b);
+  });
+
+}
+
+
 var InitialPage = React.createClass({
 	componentDidMount: function() {
 		digitalClock();
+		marquee($(".home-announcement"), $(".announcement-text"));
 	},
 
 	render: function() {
 		return (
-			<div className="home-clock"></div>
+			<div>
+				<div className="home-clock"></div>
+				<div className="home-announcement">
+					<p className="announcement-text">
+						<strong>Announcement:</strong>
+						Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.
+					</p>
+				</div>
+			</div>
 		)
 	}
 });
