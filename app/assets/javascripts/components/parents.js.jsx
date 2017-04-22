9dc886d4596e9b9f68876855ef5de65c92d5bb28
Roi Deldig <philip.deldig@bibo.com.ph>
2017-04-22 15:21:47 +0800

    updated parents UI

--- app/assets/javascripts/components/parents.js.jsx
+++ app/assets/javascripts/components/parents.js.jsx
@@ -41,40 +41,6 @@ var Parents = React.createClass({
 
     return (
       <div>
-        <div className="clearfix parent-header">
-          <div className="pull-right">
-            <span className="parent-student-id">Student ID: F100033890</span>
-          </div>
-        </div>
-
-        <div className="clearfix parent-student-information">
-          <div className="pull-left">
-            <img src="../images/dummy_101.jpg" className="parent-student-photo" />
-          </div>
-          <div className="pull-left parent-student-inf">
-            <div className="clearfix parent-inf-wrap">
-              <span className="pull-left parent-label">Name :</span>
-              <span className="pull-left parent-text">James Ivan Pilapil</span>
-            </div>
-            <div className="clearfix parent-inf-wrap">
-              <span className="pull-left parent-label">Address :</span>
-              <span className="pull-left parent-text">Fort Bonifacio, BGC, Taguig 3393</span>
-            </div>
-            <div className="clearfix parent-inf-wrap">
-              <span className="pull-left parent-label">Phone Number :</span>
-              <span className="pull-left parent-text">+639175596008</span>
-            </div>
-            <div className="clearfix parent-inf-wrap">
-              <span className="pull-left parent-label">Birthdate :</span>
-              <span className="pull-left parent-text">January 08 1999</span>
-            </div>
-            <div className="clearfix parent-inf-wrap">
-              <span className="pull-left parent-label">Email :</span>
-              <span className="pull-left parent-text">ivanpilapil@bibo.com.ph</span>
-            </div>
-          </div>
-        </div>
-
         <table className="student-checkin-table table table-striped">
           <thead>
             <tr>
