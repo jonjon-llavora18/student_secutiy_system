220df661bcac36e0d9ddd91c463c5d034ad7534e
ivan_pilapil <akosi.iban@gmail.com>
2017-04-24 13:50:30 +0800

    fix

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -17,14 +17,12 @@ class PagesController < ApplicationController
     data = StudentInformation.find_by_id(params[:id_number])
     StudentRecord.create!(description: "login", record_date: Date.current, student_information_id: data.id, name: "#{data.first_name} #{data.last_name}", record_time: DateTime.current.strftime("%H:%M:%S")) if data.present?
     session[:id_number] = data.present? ? data.id : "error"
-    uri = URI.parse("https://www.isms.com.my/isms_send.php")
-    Net::HTTP.post_form(uri, {
-      :un => 'buganblack',
-      :pwd => '123qwe123qweRR',
-      :dstno => data.guardian_contacts,
-      :msg => "#{data.first_name} #{data.last_name}, has check-in to the school",
-      :type => '1',
-    })
+    if data.present?
+      session[:id_number] = data.id
+      send_sms(data, "check-in")
+    else
+      session[:id_number] = "error"
+    end
 
     redirect_to login_user_path
   end
@@ -33,14 +31,12 @@ class PagesController < ApplicationController
     data = StudentInformation.find_by_id(params[:id_number])
     StudentRecord.create!(description: "logout", record_date: Date.current, student_information_id: data.id, name: "#{data.first_name} #{data.last_name}", record_time: DateTime.current.strftime("%H:%M:%S")) if data.present?
     session[:id_number] = data.present? ? data.id : "error"
-    uri = URI.parse("https://www.isms.com.my/isms_send.php")
-    Net::HTTP.post_form(uri, {
-      :un => 'buganblack',
-      :pwd => '123qwe123qweRR',
-      :dstno => data.guardian_contacts,
-      :msg => "#{data.first_name} #{data.last_name}, has check-out to the school",
-      :type => '1',
-    })
+    if data.present?
+      session[:id_number] = data.id
+      send_sms(data, "check-out")
+    else
+      session[:id_number] = "error"
+    end
 
     redirect_to logout_user_path
   end
@@ -50,5 +46,16 @@ class PagesController < ApplicationController
   def check_id
     return "empty" if session[:id_number].nil?
     StudentInformation.find_by_id(session[:id_number])
+  end
+
+  def send_sms(data, description)
+    uri = URI.parse("https://www.isms.com.my/isms_send.php")
+    Net::HTTP.post_form(uri, {
+      :un => 'emmanr',
+      :pwd => '123qwe123qweRR',
+      :dstno => data.guardian_contacts,
+      :msg => "#{data.first_name} #{data.last_name}, has #{description} to the school",
+      :type => '1',
+    })
   end
 end
